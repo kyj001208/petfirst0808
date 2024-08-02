adc9870104fed76f7faa6ed76fcd0b36890eda7b
songol <taeju1119@gmail.com>
2024-08-02 14:04:51 +0900

    first commit

---
+++ src/main/java/com/green/petfirst/ssh/SSHTunnellingProperties.java
@@ -0,0 +1,27 @@
+package com.green.petfirst.ssh;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("spring.ssh.tunnel")
+public class SSHTunnellingProperties {
+	
+	private String username;
+	private String sshHost;
+	private int sshPort;
+	private String privateKey;
+	//private int localPort; //config에서 random적용
+	private String rdsHost;
+	private int rdsPort;
+
+}
+
